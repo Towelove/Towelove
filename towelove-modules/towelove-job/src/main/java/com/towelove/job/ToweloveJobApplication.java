95e473467fa6a9448bad608c87c760e96f77d834
zhangjinbiao <460219753@qq.com>
2023-03-01 08:47:48 +0800

    Create Rocketmq and xxl-job module

---
+++ towelove-modules/towelove-job/src/main/java/com/towelove/job/ToweloveJobApplication.java
@@ -0,0 +1,16 @@
+package com.towelove.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: 张锦标
+ * @date: 2023/3/1 8:45
+ * ToweloveJobApplication类
+ */
+@SpringBootApplication
+public class ToweloveJobApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ToweloveJobApplication.class,args);
+    }
+}
---
+++ towelove-modules/towelove-rocket/src/main/java/com/towelove/rocket/ToweloveRocketApplication.java
@@ -0,0 +1,16 @@
+package com.towelove.rocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: 张锦标
+ * @date: 2023/3/1 8:45
+ * ToweloveRocketApplication类
+ */
+@SpringBootApplication
+public class ToweloveRocketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ToweloveRocketApplication.class,args);
+    }
+}
