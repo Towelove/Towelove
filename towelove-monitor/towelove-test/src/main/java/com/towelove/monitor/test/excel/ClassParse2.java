30697bfe99ebff5af969e175e8fce94d95d5acc1
zhangjinbiao <460219753@qq.com>
2023-04-22 14:53:29 +0800

    finish the parse of excel

--- towelove-monitor/towelove-test/src/main/java/com/towelove/monitor/test/excel/ClassParse2.java
+++ towelove-monitor/towelove-test/src/main/java/com/towelove/monitor/test/excel/ClassParse2.java
@@ -11,7 +11,6 @@ import org.springframework.util.StringUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.Arrays;
 
 /**
@@ -19,9 +18,12 @@ import java.util.Arrays;
  * @date: 2023/4/20 15:04
  * ClassParse类
  */
-public class ClassParse {
+public class ClassParse2 {
     public static void main(String[] args) {
-        String excelPath = "D:\\desktop\\table.xlsx";
+        parseToClassSchedule("D:\\desktop\\table.xlsx","2102班");
+    }
+    public static void parseToClassSchedule(String filePath,String clazzName) {
+        String excelPath = filePath;
         long beginTime = System.currentTimeMillis();
         try {
             //String encoding = "GBK";
@@ -44,29 +46,23 @@ public class ClassParse {
                 //开始解析
                 Sheet sheet = wb.getSheetAt(0);     //读取sheet 0
 
-                int firstRowIndex = sheet.getFirstRowNum() + 3;   //第一行是列名，所以不读
-                int lastRowIndex = sheet.getLastRowNum();
+                int firstRowIndex = sheet.getFirstRowNum()+1;   //第一行是列名，所以不读
+                int lastRowIndex = sheet.getLastRowNum()-1;
 //                System.out.println("firstRowIndex: "+firstRowIndex);
 //                System.out.println("lastRowIndex: "+lastRowIndex);
                 //获得班级信息
-                Row clazzName = sheet.getRow(sheet.getFirstRowNum() + 1);
-                Cell clazzInfoCell = clazzName.getCell(clazzName.getFirstCellNum());
-                System.out.println(clazzInfoCell);
-                String clazzInfo = getclazzName(clazzInfoCell);
                 for (int rIndex = firstRowIndex; rIndex <= lastRowIndex; rIndex++) {   //遍历行
 //                    System.out.println("rIndex: " + rIndex);
                     Row row = sheet.getRow(rIndex);
                     if (row != null) {
+                        //获取从星期一开始的那一列
                         int firstCellIndex = row.getFirstCellNum() + 1;
                         int lastCellIndex = row.getLastCellNum();
                         for (int cIndex = firstCellIndex; cIndex < lastCellIndex; cIndex++) {   //遍历列
                             Cell cell = row.getCell(cIndex);
                             row.getCell(cIndex);
                             if (cell != null && Strings.isNotBlank(cell.toString())) {
-//                                System.out.println(cell.toString());
-//                                System.out.println("日期----》"+cIndex);
-//                                System.out.println("节-----》"+ (rIndex - 2));
-                                toPoJo(cell, cIndex, rIndex, clazzInfo);
+                                toPoJo(cell, cIndex, rIndex, clazzName);
                             }
                         }
                     }
@@ -81,54 +77,35 @@ public class ClassParse {
         }
     }
 
-    private static String getclazzName(Cell clazzInfoCell) {
-        String clazzInfo = clazzInfoCell.toString();
-
-        int firstIndex = clazzInfo.indexOf("班级：");
-        int lastIndex = clazzInfo.indexOf("专业：");
-        String substring = clazzInfo.substring(firstIndex, lastIndex);
-        String[] split = substring.split("：");
-        String result = split[1].replace(" ", "");
-        return result;
-    }
-
-
     private static void toPoJo(Cell cell, int cIndex, int rIndex, String clazzName) {
-        String scheduleInfo = cell.toString();
-
-        String s2 = scheduleInfo.replaceAll("[\b\r\n\t]", " ").trim();
-        //System.out.println(s2);
+        String s = cell.toString();
+        String p = s.replaceAll(" P", "");
+        String s2 = p.replaceAll(" ---------------------", " ");
         String[] s1 = s2.split(" ");
         //System.out.println(Arrays.toString(s1));
-        int num = 1;
+        int num=1;
         for (int i = 0; i < s1.length; i++) {
             if (StringUtils.isEmpty(s1[i])){
                 num++;
             }
         }
-        System.out.println(num);
+        //System.out.println(num);
+
         ClassSchedule classSchedule = new ClassSchedule();
-        classSchedule.setTimeStart(rIndex - 2);
-        classSchedule.setTimeEnd(rIndex - 1);
+        classSchedule.setTimeStart(rIndex * 2 -1); //上课开始
+        classSchedule.setTimeEnd(rIndex * 2);
         classSchedule.setDay(cIndex);
         classSchedule.setClazzName(clazzName);
         for (int i=0;i<num;i++){
-            classSchedule.setTitle(s1[i*6]);
-            classSchedule.setTeacher(s1[i*6+1]);
-            classSchedule.setWeek(s1[i*6+2]);
-            classSchedule.setLocation(s1[i*6+3]);
+            classSchedule.setTitle(s1[i*5]);
+            classSchedule.setTeacher(s1[i*5+1]);
+
+            String week = s1[i * 5 + 2];
+            String weekfrom = week.substring(0, week.indexOf("("));
+            classSchedule.setWeek(weekfrom);
+            classSchedule.setLocation(s1[i*5+3]);
             //TODO 插入数据库
             System.out.println(classSchedule);
         }
-
-
-        //String[] infos = scheduleInfo.split("\n");
-        //classSchedule.setTitle(infos[1]);
-        //classSchedule.setTeacher(infos[2]);
-        //classSchedule.setWeek(infos[3]);
-        //classSchedule.setLocation(infos[4]);
-
-
-        //TODO 插入数据库
     }
 }
--- towelove-monitor/towelove-test/src/main/java/com/towelove/monitor/test/excel/MulClass.java
+++ towelove-monitor/towelove-test/src/main/java/com/towelove/monitor/test/excel/MulClass.java
@@ -11,25 +11,36 @@ import java.util.Arrays;
  */
 public class MulClass {
     public static void main(String[] args) {
-        String s = "概率论与数理统计\n" +
-                "董宁(副教授)\n" +
-                "1-6[周]\n" +
-                "公共105\n" +
-                "[09-10]节\n" +
-                "\n" +
-                "电路与电子学\n" +
-                "刘颖慧(副教授)\n" +
-                "1-10[周]\n" +
-                "公共109\n" +
-                "[09-10]节\n";
-        String s2 = s.replaceAll("[\b\r\n\t]", " ");
-        System.out.println(s2);
+        String s = "就业指导 夏叶文编审 4,6-7(周)[09-10节] 公共423 --------------------- 就业指导 P 夏叶文编审 8(周)[09-10节] 公共423";
+        String p = s.replaceAll(" P", "");
+        String s2 = p.replaceAll(" ---------------------", " ");
         String[] s1 = s2.split(" ");
         System.out.println(Arrays.toString(s1));
-        for (String s3 : s1) {
-            if (StringUtils.isEmpty(s3)){
-                System.out.println("---"+s3+"---");
+        int num=1;
+        for (int i = 0; i < s1.length; i++) {
+            if (StringUtils.isEmpty(s1[i])){
+                num++;
             }
+        }
+        System.out.println(num);
+        int rIndex=1;
+        int cIndex=1;
+        String clazzName = "2002班";
+        ClassSchedule classSchedule = new ClassSchedule();
+        classSchedule.setTimeStart(rIndex * 2 -1); //上课开始
+        classSchedule.setTimeEnd(rIndex * 2);
+        classSchedule.setDay(cIndex);
+        classSchedule.setClazzName(clazzName);
+        for (int i=0;i<num;i++){
+            classSchedule.setTitle(s1[i*5]);
+            classSchedule.setTeacher(s1[i*5+1]);
+
+            String week = s1[i * 5 + 2];
+            String weekfrom = week.substring(0, week.indexOf("("));
+            classSchedule.setWeek(weekfrom);
+            classSchedule.setLocation(s1[i*5+3]);
+            //TODO 插入数据库
+            System.out.println(classSchedule);
         }
     }
 }
