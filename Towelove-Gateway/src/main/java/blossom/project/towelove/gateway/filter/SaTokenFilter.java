fde02f245c169f28818e26a913acf7f514402542
link <link.ji@careerfocus.com.cn>
2023-11-21 16:24:11 +0800

    "补充基础设施"

---
+++ Towelove-Gateway/src/main/java/blossom/project/towelove/gateway/filter/SaTokenFilter.java
@@ -0,0 +1,48 @@
+package blossom.project.towelove.gateway.filter;
+
+import cn.dev33.satoken.reactor.filter.SaReactorFilter;
+import cn.dev33.satoken.router.SaRouter;
+import cn.dev33.satoken.stp.StpUtil;
+import cn.dev33.satoken.util.SaResult;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import java.util.stream.Collectors;
+
+public class SaTokenFilter {
+    @Bean
+    public SaReactorFilter getSaReactorFilter() {
+        return new SaReactorFilter()
+                // 拦截地址
+                .addInclude("/**")    /* 拦截全部path */
+                // 开放地址
+                .addExclude("/favicon.ico")
+                // 鉴权方法：每次访问进入
+                .setAuth(obj -> {
+                    // 登录校验 -- 拦截所有路由，并排除/auth/** 用于开放登录
+                    SaRouter.match("/**", "/auth/**", r -> StpUtil.checkLogin());
+                    // 权限认证 -- 不同模块, 校验不同权限
+                    SaRouter.match("/user/**", r -> StpUtil.checkPermission("passenger"));
+                    SaRouter.match("/admin/**", r -> StpUtil.checkPermission("admin"));
+//                  SaRouter.match("/orders/**", r -> StpUtil.checkPermission("orders"));
+// 更多匹配 ...  */
+                })
+                // 异常处理方法：每次setAuth函数出现异常时进入
+                .setError(e -> {
+                    return SaResult.error(e.getMessage());
+                });
+    }
+
+    /**
+     * 由于网关没有引入springMVC依赖，所以使用feign的时候需要手动装配messageConverters
+     * @param converters
+     * @return
+     */
+    @Bean
+    public HttpMessageConverters messageConverters(ObjectProvider<HttpMessageConverter<?>> converters) {
+        return new HttpMessageConverters(converters.orderedStream().collect(Collectors.toList()));
+    }
+}
