bd6ea16efca8ed030a9cfbb93602feb8cec379f3
jinbiao.zhang <460219753@qq.com>
2023-11-30 14:25:52 +0800

    项目结构

---
+++ Towelove-Loves/src/main/java/blossom/project/towelove/loves/ToweloveLovesApplication.java
@@ -0,0 +1,32 @@
+package blossom.project.towelove.loves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author: ZhangBlossom
+ * @date: 2023/11/30 13:47
+ * @contact: QQ:4602197553
+ * @contact: WX:qczjhczs0114
+ * @blog: https://blog.csdn.net/Zhangsama1
+ * @github: https://github.com/ZhangBlossom
+ * ToweloveLovesApplication类
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients("blossom.project.towelove.client")
+public class ToweloveLovesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ToweloveLovesApplication.class,args);
+        System.out.println("（づ￣3￣）づ╭❤～ Towelove Loves模块启动成功  (o>ε(o>ｕ(≧∩≦)");
+        System.out.println(
+                " _                         \n" +
+                        "| |                        \n" +
+                        "| |     _____   _____  ___ \n" +
+                        "| |    / _ \\ \\ / / _ \\/ __|\n" +
+                        "| |___| (_) \\ V /  __/\\__ \\\n" +
+                        "|______\\___/ \\_/ \\___||___/\n");
+    }
+}
