75a63dcc4b3a463d3e5182d8b4386497acc9c425
jinbiao.zhang <460219753@qq.com>
2023-12-03 21:22:29 +0800

    准备开发相册服务

---
+++ Towelove-Loves/src/main/java/blossom/project/towelove/loves/controller/LoveSubAlbumController.java
@@ -0,0 +1,22 @@
+package blossom.project.towelove.loves.controller;
+
+import blossom.project.towelove.framework.log.annotation.LoveLog;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: ZhangBlossom
+ * @date: 2023/12/3 21:22
+ * @contact: QQ:4602197553
+ * @contact: WX:qczjhczs0114
+ * @blog: https://blog.csdn.net/Zhangsama1
+ * @github: https://github.com/ZhangBlossom
+ * LoveSubAlbumController类
+ */
+@LoveLog
+@RestController
+@RequestMapping("/v1/love/sub/album")
+@RequiredArgsConstructor
+public class LoveSubAlbumController {
+}
