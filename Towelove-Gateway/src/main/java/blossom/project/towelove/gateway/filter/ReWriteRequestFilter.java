51c7da2ca065a9e61fe6b951682a60704127fa77
jinbiao.zhang <your_email@example.com>
2023-12-10 14:44:28 +0800

    简单修改网关内容

--- Towelove-Gateway/src/main/java/blossom/project/towelove/gateway/filter/ReWriteRequestFilter.java
+++ Towelove-Gateway/src/main/java/blossom/project/towelove/gateway/filter/ReWriteRequestFilter.java
@@ -4,13 +4,16 @@ import blossom.project.towelove.common.constant.TokenConstant;
 import cn.dev33.satoken.stp.StpUtil;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class ReWriteRequestFilter implements GlobalFilter {
+public class ReWriteRequestFilter implements GlobalFilter , Ordered {
     /**
      * 重写request 携带userId分发到后续请求
      * @param exchange the current server exchange
@@ -23,7 +26,11 @@ public class ReWriteRequestFilter implements GlobalFilter {
         try{
              id = StpUtil.getLoginId().toString();
         }catch (Exception e){
-            return chain.filter(exchange);
+            //如果请求没有loginId就报错并且直接返回原因
+            //设定返回内容和返回状态码
+            DataBuffer dataBuffer =exchange.getResponse().bufferFactory().wrap(e.getMessage().getBytes());
+            exchange.getResponse().setStatusCode(HttpStatus.NON_AUTHORITATIVE_INFORMATION);
+            return exchange.getResponse().writeWith(Mono.just(dataBuffer));
         }
         //重写请求
         ServerHttpRequest requst = exchange
@@ -31,5 +38,10 @@ public class ReWriteRequestFilter implements GlobalFilter {
                 .mutate()
                 .header(TokenConstant.USER_ID_HEADER, id).build();
         return chain.filter(exchange.mutate().request(requst).build());
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
     }
 }
