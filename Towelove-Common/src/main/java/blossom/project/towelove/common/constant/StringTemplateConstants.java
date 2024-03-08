10b96a1d8d9bae6e9017f5c74e31c442318488cd
link <link.ji@careerfocus.com.cn>
2024-03-08 21:55:19 +0800

    "新增伴侣邀请二维码接口"

--- Towelove-Common/src/main/java/blossom/project/towelove/common/constant/StringTemplateConstants.java
+++ Towelove-Common/src/main/java/blossom/project/towelove/common/constant/StringTemplateConstants.java
@@ -11,9 +11,24 @@ package blossom.project.towelove.common.constant;
  * 模板字符串
  */
 public class StringTemplateConstants {
-    private static final String invitedTemplate = "用户：%s邀请您加入towelove，点击链接【%s】同意他的请求吧！";
+    private static final String COMMON_INVITED_TEMPLATE = "用户：%s邀请您加入towelove，点击链接【https://web.towelove.cn/?invitedCode=%s】同意他的请求吧！";
+    private static final String QRCODE_INVITED_TEMPLATE = "https://web.towelove.cn/?invited=%s";
 
-    public static String getInvitedTemplate(String userName,String shortUrl){
-        return String.format(invitedTemplate,userName,shortUrl);
+    public static final String QRCODE_TYPE = "qrCode";
+
+    public static final String COMMON_TYPE = "common";
+
+
+
+
+
+    public static String getInvitedTemplate(String userName,String invitedCode,String type){
+        String template = "";
+        switch (type){
+            case QRCODE_TYPE -> template = String.format(QRCODE_INVITED_TEMPLATE,invitedCode);
+            case COMMON_TYPE -> template = String.format(COMMON_INVITED_TEMPLATE,userName,invitedCode);
+            default -> template = String.format(COMMON_INVITED_TEMPLATE,userName,invitedCode);
+        }
+        return template;
     }
 }
